ce4ea2b41d37668ac540aa9077f8a301b70f988f
TimothyNgoi98 <66301025+TimothyNgoi98@users.noreply.github.com>
2023-03-16 23:33:32 +0800

    changes to card and button

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Card, Grid, Typography, Box } from "@mui/material";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+
 const Card1 = () => {
     return(
         <Grid container pt={2} pb = {1}>
             <Card sx={{backgroundColor:'background.card', width:'95%', margin:'auto'}}>
-                <Grid item xs={8}>
-                    <Box sx={{flexDirection:'column'}} ml = {1} p={1}>
-                        <Typography variant='subtitle1'>Salary Rights</Typography>
-                        <Typography variant='subtitle1'>বেতন অধিকার</Typography>
-                    </Box>
+                <Grid container direction='row' justifyContent="space-between">
+                    <Grid item xs={9}>
+                        <Box sx={{flexDirection:'column'}} ml = {1} p={1}>
+                            <Typography variant='subtitle1'>Salary Rights</Typography>
+                            <Typography variant='subtitle1'>বেতন অধিকার</Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={3} align='center'>
+                        <Box mt = {3}>
+                            <PlayArrowIcon color="primary"/>
+                        </Box>
+                    </Grid>
                 </Grid>
             </Card>
         </Grid>
