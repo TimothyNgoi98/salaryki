c5d19f9acae421eea1ac80dee497d05fe5e87868
TimothyNgoi98 <66301025+TimothyNgoi98@users.noreply.github.com>
2023-04-11 19:36:02 +0800

    edits

--- src/components/NoticePeriodCard/index.js
+++ src/components/NoticePeriodCard/index.js
@@ -2,6 +2,25 @@ import React from "react";
 import { useState } from 'react';
 import { Card, Grid, Typography, Box, Collapse, MobileStepper, Button } from "@mui/material";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+
+// const steps = [
+//     {
+//         labelEnglish:"Singapore public holidays",
+//         labelIndian:"সিঙ্গাপুর পাবলিক ছুটি",
+//         image: nationalDay,
+//         descriptionEnglish:"Singapore has 11 public holidays a year. You will be paid even if not working on PH!",
+//         descriptionIndian:"সিঙ্গাপুরে এক বছরে 11 টি পাবলিক ছুটি রয়েছে। P.H. এ কাজ না করলেও আপনাকে বেতন দেওয়া হবে!",
+//     },
+//     {
+//         labelEnglish:"Public holiday pay",
+//         labelIndian:"সরকারী ছুটি বেতন",
+//         image: phPay,
+//         descriptionEnglish:"If you are working on P.H., you get extra day of salary.",
+//         descriptionIndian:"আপনি যদি সরকারী ছুটিতে কাজ করেন তবে আপনাকে বেতনের অতিরিক্ত দিন দেওয়া হবে",
+//     }
+// ]
 
 function NoticePeriodCard() {
 
@@ -10,6 +29,19 @@ function NoticePeriodCard() {
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
+
+    //steps
+    // const [activeStep, setActiveStep] = useState(0);
+    // const maxSteps = steps.length;
+
+    // const handleNext = () => {
+    //     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    // };
+
+    // const handleBack = () => {
+    //     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    // };
+    
 
     return (
         <Grid container pt={2} pb={1}>
@@ -38,6 +70,58 @@ function NoticePeriodCard() {
                     </Grid>
                 </Grid>
             </Card>
+            {/* <Collapse in={expanded} sx={{ width: '95%', margin: 'auto' }}>
+                <Grid container sx={{ border: 'solid #6D4B38', borderRadius: '6px' }} mt={2} justifyContent="center" alignItems="center">
+                    <Box sx={{ flexDirection: 'column' }}>
+                        <Grid container
+                            direction="column"
+                            justifyContent="center"
+                            alignItems="center">
+                            <Typography variant="h6" mt={2} fontWeight="bold">
+                                {steps[activeStep].labelEnglish}
+                            </Typography>
+                            <Typography variant="h6" fontWeight="bold">
+                                {steps[activeStep].labelIndian}
+                            </Typography>
+                            <Box component="img" src={steps[activeStep].image} mt={2} sx={{ borderRadius: "6px" }} />
+                            <Grid pt={1} p={3}>
+                                <Typography variant="subtitle1">
+                                    {steps[activeStep].descriptionEnglish}
+                                </Typography>
+                                <br />
+                                <Typography variant="subtitle1">
+                                    {steps[activeStep].descriptionIndian}
+                                </Typography>
+                            </Grid>
+                            <MobileStepper
+                                variant="progress"
+                                steps={maxSteps}
+                                position="static"
+                                activeStep={activeStep}
+                                sx={{ maxWidth: 800, flexGrow: 1 }}
+                                backButton={
+                                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                                        {activeStep === 0 ?
+                                            <KeyboardArrowLeft sx={{ display: 'none', color: "black" }} />
+                                            :
+                                            <KeyboardArrowLeft sx={{ color: "black" }} />
+                                        }
+                                    </Button>
+                                }
+                                nextButton={
+                                    <Button size="small" onClick={handleNext} disabled={activeStep === (maxSteps - 1)}>
+                                        {activeStep === (maxSteps - 1) ?
+                                            <KeyboardArrowRight sx={{ display: 'none', color: "black" }} />
+                                            :
+                                            <KeyboardArrowRight sx={{ color: "black" }} />
+                                        }
+                                    </Button>
+                                }
+                            />
+                        </Grid>
+                    </Box>
+                </Grid>
+            </Collapse> */}
         </Grid>
     )
 }
