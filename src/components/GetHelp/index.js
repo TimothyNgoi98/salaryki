b9c010b5fdfc5caaee90f1f37fd26f07f5ea0524
TimothyNgoi98 <66301025+TimothyNgoi98@users.noreply.github.com>
2023-04-15 22:45:30 +0800

    edits

--- src/components/GetHelp/index.js
+++ src/components/GetHelp/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Grid, Paper, Box, Typography } from "@mui/material";
+import { Grid, Paper, Box, Typography, Link } from "@mui/material";
 // import salaryFlow from '../../images/salaryFlow.jpg'
-import {ReactComponent as SalaryFlowSVG} from '../../images/salaryFlow2.svg'
+import { ReactComponent as SalaryFlowSVG } from '../../images/salaryFlow2.svg'
 import bestBros from '../../images/bestBros.jpg'
 import callMe from '../../images/callMe.jpg'
 import locationUnknown from '../../images/locationUnknown.jpg'
@@ -15,8 +15,8 @@ function GetHelp() {
         <>
             <Grid container pt={4} spacing={2} sx={{ maxWidth: '85%', margin: 'auto' }}>
                 <Paper sx={{ width: '100%', height: 'auto', borderRadius: '8px' }} elevation={4} mb={2} >
-                    <Grid container alignContent="center" justifyContent="center" pb = {1}>
-                        <SalaryFlowSVG sx={{ width: '100%', height: '100%' }}/>
+                    <Grid container alignContent="center" justifyContent="center" pb={1}>
+                        <SalaryFlowSVG />
                         {/* <Box component="img" src={salaryFlow} sx={{ width: '100%' }} p={1} /> */}
                     </Grid>
                 </Paper>
@@ -30,7 +30,7 @@ function GetHelp() {
                         {/* <BestBrosSVG/> */}
                         {/* <SvgIcon component={BestBrosSVG} viewBox="0 0 100 100"/> */}
                         <Box component="img" src={bestBros} sx={{ maxWidth: '90%', borderRadius: '6px' }} p={1} mt={1} />
-                        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', alignContent: 'space-around' }} mt={2} mb={1}>
+                        {/* <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', alignContent: 'space-around' }} mt={2} mb={1}>
                             <Box component="img" src={callMe} sx={{ maxWidth: '32px', maxHeight:"32px"}} mr={2} />
                             <Box sx={{ flexDirection: 'column' }}>
                                 <Typography variant='subtitle1'>Call/WhatsApp us now</Typography>
@@ -59,22 +59,55 @@ function GetHelp() {
                             <Box sx={{ flexDirection: 'column', alignItems: 'center' }}>
                                 <Typography variant='subtitle1'>TWC2 Facebook (Bangla)</Typography>
                             </Box>
-                        </Box>
-                        {/* <Grid container direction='row' justifyContent="space-between" sx={{ maxWidth: '90%'}}>
-                            <Grid container item xs= {4} alignItems="center" justifyContent="flex-end">
-                                <Box component="img" src={callMe}  p={1} mt={1}/>
+                        </Box> */}
+                        <Grid container direction='row' justifyContent="space-between" sx={{ maxWidth: '90%', mb:2, mt: 2}}>
+                            <Grid container item xs={4} alignItems="center" justifyContent="flex-end">
+                                <Box component="img" src={callMe} sx={{ maxWidth: '32px', maxHeight: "32px" }} mr={2} />
                             </Grid>
-                            <Grid item xs = {8}>
+                            <Grid item xs={8} mb = {4}>
                                 <Box sx={{ flexDirection: 'column' }}>
-                                    <Typography variant='subtitle1'>Call/WhatsApp us now</Typography>
+                                    <Typography variant='subtitle1' sx = {{fontWeight:'bold', mb:1}}>Call/WhatsApp us now</Typography>
+
                                     <Typography variant='subtitle1'>+65 6297 7564</Typography>
                                 </Box>
                             </Grid>
-                        </Grid> */}
+                            <Grid container item xs={4} alignItems="center" justifyContent="flex-end">
+                                <Box component="img" src={locationUnknown} sx={{ maxWidth: '32px', maxHeight: "46px" }} mr={2} />
+                            </Grid>
+                            <Grid item xs={8} mb = {4}>
+                            <Box sx={{ flexDirection: 'column' }}>
+                                <Typography variant='subtitle1' sx = {{fontWeight:'bold', mb:1}}>TWC2 Office</Typography>
+                                <Typography variant='subtitle1'>Mondays to Fridays from 9am to 5pm</Typography>
+                                <Typography variant='subtitle1'>180B Bencoolen Street #09-01</Typography>
+                                <Typography variant='subtitle1'>The Bencoolen</Typography>
+                                <Typography variant='subtitle1'>Singapore 189648</Typography>
+                            </Box>
+                            </Grid>
+                            <Grid container item xs={4} alignItems="center" justifyContent="flex-end" mb = {4}>
+                                <Box component="img" src={www} sx={{ maxWidth: '42px', maxHeight: "42px" }} mr={2} />
+                            </Grid>
+                            <Grid item xs={8}>
+                            <Box sx={{ flexDirection: 'column', alignItems:"center", justifyContent:"center" }}>
+                                <Link href="https://twc2.org.sg/language-menu-for-qr-code/" target="_blank" sx={{ color: "#0065FD", textDecoration: 'underline' }}>
+                                    <Typography variant='subtitle1'>TWC2 Website</Typography>
+                                </Link>
+                            </Box>
+                            </Grid>
+                            <Grid container item xs={4} alignItems="center" justifyContent="flex-end">
+                                <Box component="img" src={facebook} sx={{ maxWidth: '42px', maxHeight: "42px" }} mr={2} />
+                            </Grid>
+                            <Grid item xs={8} mb = {4}>
+                            <Box sx={{ flexDirection: 'column' }}>
+                                <Link href="https://www.facebook.com/twc2bangla" target="_blank" sx={{ color: "#0065FD", textDecoration: 'underline' }}>
+                                    <Typography variant='subtitle1'>TWC2 Facebook (Bangla)</Typography>
+                                </Link>
+                            </Box>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Paper>
             </Grid>
-            <Grid container pt={4} pb = {2} spacing={2} sx={{ maxWidth: '85%', margin: 'auto' }}>
+            <Grid container pt={4} pb={2} spacing={2} sx={{ maxWidth: '85%', margin: 'auto' }}>
                 <Paper sx={{ width: '100%', height: 'auto', borderRadius: '8px' }} elevation={4} mb={2}>
                     {/* <Grid container alignContent='center' justifyContent='center'>
                         <Box component="img" src={salaryFlow} sx={{ width: '100%' }} p={1} />
@@ -83,18 +116,18 @@ function GetHelp() {
                         justifyContent="center"
                         alignItems="center" pt={2}>
                         <Typography variant="h6" sx={{ fontWeight: 'bold' }}>About SalaryKi</Typography>
-                        <Grid container p = {3} alignItems="center" justifyContent="center">
+                        <Grid container p={3} alignItems="center" justifyContent="center">
                             <Typography variant="subtitle1">
-                                SalaryKi is a website built by Lucas, a final year student from the National University of Singapore's 
-                                Division of Industrial Design as part of his final year thesis. SalaryKi is the result of months of volunteering and collaboration with TWC2, and 
+                                SalaryKi is a website built by Lucas, a final year student from the National University of Singapore's
+                                Division of Industrial Design as part of his final year thesis. SalaryKi is the result of months of volunteering and collaboration with TWC2, and
                                 aims to present important salary and employment information accessibly to migrant workers.
                             </Typography>
-                            <Typography variant="subtitle1" mt = {3}>
-                                SalaryKi হল একটি ওয়েবসাইট যা Lucas দ্বারা নির্মিত, ন্যাশনাল ইউনিভার্সিটি অফ সিঙ্গাপুরের ডিভিশন অফ ইন্ডাস্ট্রিয়াল ডিজাইনের 
-                                শেষ বর্ষের ছাত্র তার শেষ বছরের থিসিসের অংশ হিসাবে। SalaryKi হল কয়েক মাসের স্বেচ্ছাসেবী এবং TWC2 এর সাথে সহযোগিতার ফল, এবং অভিবাসী কর্মীদের কাছে 
+                            <Typography variant="subtitle1" mt={3}>
+                                SalaryKi হল একটি ওয়েবসাইট যা Lucas দ্বারা নির্মিত, ন্যাশনাল ইউনিভার্সিটি অফ সিঙ্গাপুরের ডিভিশন অফ ইন্ডাস্ট্রিয়াল ডিজাইনের
+                                শেষ বর্ষের ছাত্র তার শেষ বছরের থিসিসের অংশ হিসাবে। SalaryKi হল কয়েক মাসের স্বেচ্ছাসেবী এবং TWC2 এর সাথে সহযোগিতার ফল, এবং অভিবাসী কর্মীদের কাছে
                                 গুরুত্বপূর্ণ বেতন এবং কর্মসংস্থানের তথ্য সহজলভ্যভাবে উপস্থাপন করার লক্ষ্য
                             </Typography>
-                            <Box component="img" src = {footer} mt = {3} sx={{maxWidth:'100%'}}/>
+                            <Box component="img" src={footer} mt={3} sx={{ maxWidth: '100%' }} />
                         </Grid>
                     </Grid>
                 </Paper>
